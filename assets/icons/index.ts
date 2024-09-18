e2817d49d5b28eee8feb976f7fff40b5bc2bd266
ankushh14 <ankushshenoy97@gmail.com>
2024-09-19 00:05:25 +0530

    feat: add SVG icons as JSX components

---
+++ assets/icons/index.ts
@@ -0,0 +1,6 @@
+import ArrowDown from "./ArrowDown";
+import ArrowRight from "./ArrowRight";
+import MagicPen from "./MagicPen";
+import Regenerate from "./Regenerate";
+
+export { ArrowDown, ArrowRight, MagicPen, Regenerate };
