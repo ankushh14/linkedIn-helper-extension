0a95e80647620f26f271fcc2c5a90da56d16fff4
ankushh14 <ankushshenoy97@gmail.com>
2024-09-19 22:38:19 +0530

    style: add modalBg color and pop-in animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./entrypoints/**/*.{html,tsx,ts}", "./components/**/*.{tsx,ts}"],
+  content: ["./entrypoints/**/*.{html,tsx,ts}", "./ui/**/*.{tsx,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        modalBg: "#F9FAFB",
+      },
+      keyframes: {
+        "pop-in": {
+          from: { scale: "0" },
+          to: { scale: "1" },
+        },
+      },
+      animation: {
+        "pop-in": "pop-in 0.5s ease-in-out forwards",
+      },
+    },
   },
   plugins: [],
 };
