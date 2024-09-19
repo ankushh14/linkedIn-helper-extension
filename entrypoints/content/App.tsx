c9c9e1b8ede074f6a7a3c635f3c8276064cf6393
ankushh14 <ankushshenoy97@gmail.com>
2024-09-19 23:54:43 +0530

    feat: create helper button everytime input text field is focused

--- entrypoints/content/App.tsx
+++ entrypoints/content/App.tsx
@@ -1,14 +1,45 @@
-import React from "react";
+import { LINKEDIN_MSG_CONTAINER } from "@/constants";
+import { useEffect, useState } from "react";
+import CreateHelperButton from "./components/CreateHelperButton";
+import PromptModal from "./components/PromptModal";
 
 const App = () => {
-  const [count, setCount] = React.useState(1);
-  const increment = () => setCount((count) => count + 1);
+  const [modal, setModal] = useState(false);
+  const [focusedElement, setFocusedElement] = useState<HTMLElement | null>(
+    null
+  );
+
+  useEffect(() => {
+    const handleFocusIn = (event: Event) => {
+      const target = event.target as HTMLElement;
+      if (target && target.matches(LINKEDIN_MSG_CONTAINER)) {
+        setFocusedElement(target);
+      }
+    };
+
+    const handleFocusOut = (event: Event) => {
+      const target = event.target as HTMLElement;
+      if (target && !target.matches(LINKEDIN_MSG_CONTAINER)) {
+        setFocusedElement(null);
+      }
+    };
+
+    document.addEventListener("focusin", handleFocusIn);
+    document.addEventListener("focusout", handleFocusOut);
+
+    return () => {
+      document.removeEventListener("focusin", handleFocusIn);
+      document.removeEventListener("focusout", handleFocusOut);
+    };
+  }, []);
 
   return (
-    <div className="bg-red-500">
-      <p>This is React. {count}</p>
-      <button onClick={increment}>Increment</button>
-    </div>
+    <>
+      {focusedElement && (
+        <CreateHelperButton container={focusedElement} setModal={setModal} />
+      )}
+      <PromptModal modal={modal} setModal={setModal} />
+    </>
   );
 };
 
