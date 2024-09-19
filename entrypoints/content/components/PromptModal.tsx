a3d63024db1b77eb97f374b284e90f7c6196d0d0
ankushh14 <ankushshenoy97@gmail.com>
2024-09-19 23:53:22 +0530

    feat: create prompt modal component

---
+++ entrypoints/content/components/PromptModal.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import PromptModalBody from "./PromptModalBody";
+
+type PromptModalProps = {
+  modal: boolean;
+  setModal: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const PromptModal = ({ modal, setModal }: PromptModalProps) => {
+  const modalRef = React.useRef<HTMLDivElement>(null);
+  const messageContainerRef = React.useRef<HTMLElement>();
+
+  const closeModal = () => {
+    setModal(false);
+  };
+
+  const handleExitOnEscape = (e: KeyboardEvent) => {
+    if (e.key.toLowerCase() === "escape") {
+      closeModal();
+    }
+  };
+
+  React.useEffect(() => {
+    addEventListener("keydown", handleExitOnEscape);
+    return () => removeEventListener("keydown", handleExitOnEscape);
+  });
+
+  React.useEffect(() => {
+    if (modal) {
+      const modalElement = modalRef.current;
+
+      const handleFocusTrap = (e: KeyboardEvent) => {
+        const focusableElements = modalRef?.current?.querySelectorAll(
+          'a[href], button, textarea, input, select, [tabindex]:not([tabindex="-1"])'
+        ) as NodeListOf<HTMLElement>;
+        const firstElement = focusableElements[0];
+        const lastElement = focusableElements[focusableElements.length - 1];
+
+        if (e.key === "Tab") {
+          if (e.shiftKey) {
+            if (document.activeElement === firstElement) {
+              e.preventDefault();
+              lastElement.focus();
+            }
+          } else {
+            if (document.activeElement === lastElement) {
+              e.preventDefault();
+              firstElement.focus();
+            }
+          }
+        }
+      };
+
+      if (document.activeElement instanceof HTMLElement) {
+        const helperButton = document.activeElement as HTMLElement;
+        messageContainerRef.current =
+          helperButton.previousElementSibling as HTMLElement;
+      }
+
+      const focusableElements = modalElement?.querySelectorAll(
+        'a[href], button, textarea, input, select, [tabindex]:not([tabindex="-1"])'
+      ) as NodeListOf<HTMLElement>;
+      if (focusableElements.length) {
+        focusableElements[0].focus();
+      }
+
+      window.addEventListener("keydown", handleFocusTrap);
+      return () => {
+        window.removeEventListener("keydown", handleFocusTrap);
+      };
+    }
+  }, [modal]);
+
+  return (
+    <div
+      className={`w-full fixed z-[9999] bg-[#0D0D1233] top-0 left-0 right-0 bottom-0 justify-center items-center ${modal ? "flex" : "hidden"}`}
+      onClick={closeModal}
+      role="dialog"
+      aria-modal={modal ? "true" : "false"}
+      ref={modalRef}
+    >
+      <PromptModalBody
+        messageContainerRef={messageContainerRef}
+        setModal={setModal}
+      />
+    </div>
+  );
+};
+
+export default PromptModal;
---
+++ entrypoints/content/components/PromptModalBody.tsx
@@ -0,0 +1,113 @@
+import { ArrowDown, ArrowRight, RegenerateIcon } from "@/assets/icons";
+import { LINKEDIN_PLACEHOLDER_CLASS } from "@/constants";
+import Button from "@/ui/button";
+import { getResponse } from "@/utils/generatedummyresponse";
+import { FormEvent, useState } from "react";
+import ChatAIComponent, { Chat } from "./ChatAIComponent";
+
+const PromptModalBody = ({
+  messageContainerRef,
+  setModal,
+}: Readonly<{
+  messageContainerRef: React.MutableRefObject<HTMLElement | undefined>;
+  setModal: React.Dispatch<React.SetStateAction<boolean>>;
+}>) => {
+  const [inputValue, setInputValue] = useState("");
+  const [chats, setChats] = useState<Chat[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [secondaryBtns, setSecondaryBtns] = useState(false);
+  const [generatedResponse, setGeneratedResponse] = useState("");
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!inputValue.length) {
+      return;
+    }
+    setChats((prev) => [...prev, { text: inputValue, author: "user" }]);
+    setInputValue("");
+    setLoading(true);
+    const response = await getResponse({
+      text: inputValue,
+    });
+    if (response) {
+      setLoading(false);
+      setSecondaryBtns(true);
+      setGeneratedResponse(response.text);
+      setChats((prev) => [...prev, response]);
+    }
+  };
+
+  const InjectTextHandler = () => {
+    messageContainerRef.current?.parentElement?.nextElementSibling?.classList.remove(
+      LINKEDIN_PLACEHOLDER_CLASS
+    );
+    messageContainerRef.current!.innerText = generatedResponse;
+    setChats([]);
+    setSecondaryBtns(false);
+    setGeneratedResponse("");
+    setModal(false);
+  };
+
+  return (
+    <article
+      className="w-full max-w-[600px] bg-modalBg rounded-2xl flex flex-col items-center p-7 gap-4 overflow-hidden text-gray-500"
+      onClick={(e) => e.stopPropagation()}
+    >
+      <ChatAIComponent chats={chats} loadingState={loading} />
+      <form
+        onSubmit={handleSubmit}
+        className="w-full flex flex-col items-end gap-4"
+      >
+        <input
+          type="text"
+          className="w-full border border-gray-300 focus:border-gray-500 placeholder:text-gray-300 p-3 outline-none rounded-md"
+          placeholder="Your prompt here"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        {secondaryBtns ? (
+          <SecondaryBtns injectHandler={InjectTextHandler} />
+        ) : (
+          <Button
+            type="submit"
+            className="w-fit flex justify-center items-center gap-3"
+            disabled={loading}
+          >
+            <ArrowRight width="15" height="15" />
+            Generate
+          </Button>
+        )}
+      </form>
+    </article>
+  );
+};
+
+type SecondaryBtnsProps = {
+  injectHandler: () => void;
+};
+
+const SecondaryBtns = ({ injectHandler }: SecondaryBtnsProps) => {
+  return (
+    <div className="flex gap-4 justify-center items-center">
+      <Button
+        variant="outline"
+        type="button"
+        className="w-fit flex justify-center items-center gap-2"
+        onClick={injectHandler}
+      >
+        <ArrowDown width="15" height="15" />
+        Insert
+      </Button>
+      <Button
+        type="button"
+        disabled
+        className="w-fit flex justify-center items-center gap-1"
+      >
+        <RegenerateIcon width="15" height="15" />
+        Regenerate
+      </Button>
+    </div>
+  );
+};
+
+export default PromptModalBody;
