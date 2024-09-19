b8110b4fdbe19a6ec6e2a871a3c76e61bb9b298a
ankushh14 <ankushshenoy97@gmail.com>
2024-09-19 22:37:50 +0530

    feat: add constants.ts for reusable values

---
+++ constants.ts
@@ -0,0 +1,15 @@
+export const LINKEDIN_MSG_CONTAINER = ".msg-form__contenteditable";
+
+export const LINKEDIN_PLACEHOLDER_CLASS = "msg-form__placeholder";
+
+export const INSTRUCTIONS = [
+  "Click on the Helper Button, which appears when you focus on a text input field. The button will become visible once the text field is active.",
+  "A modal window will open. In the modal, type a prompt describing how you would like the reply to be generated. This can be a brief description of the type of response you're looking for.",
+  "Once the reply is generated, you can either click the Insert button to place the generated reply into the text field or click the Regenerate button to create a new reply if you're not satisfied with the current one.",
+];
+
+export const DESCRIPTION =
+  "This extension helps users generate thoughtful and relevant replies to linkedIn messages.";
+
+export const CHAT_REPLY =
+  "Thank you for the opportunity! If you have any more questions or if there's anything else I can help you with, feel free to ask.";
