ce97fb38f909faf2a7ae9e7ce62000e63c90d210
ankushh14 <ankushshenoy97@gmail.com>
2024-09-19 23:50:17 +0530

    feat: warning message using chrome.tabs api

--- entrypoints/popup/components/PopupBody.tsx
+++ entrypoints/popup/components/PopupBody.tsx
@@ -1,14 +1,12 @@
+import { DESCRIPTION, INSTRUCTIONS } from "@/constants";
 import React from "react";
-import instructions from "../data/basic-instructions";
 
 const PopupBody = () => {
   return (
     <>
       <header className="w-full flex flex-col justify-center items-center p-3 gap-1">
         <h1 className="text-2xl font-semibold">LinkedIn AI Reply</h1>
-        <p className="text-sm text-slate-500 text-center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, maiores.
-        </p>
+        <p className="text-sm text-slate-500 text-center">{DESCRIPTION}</p>
       </header>
       <Instructions />
     </>
@@ -17,16 +15,46 @@ const PopupBody = () => {
 
 const Instructions = () => {
   const [notLinkedIn, setNotLinkedIn] = React.useState(false);
+
+  const checkUrl = React.useCallback(async () => {
+    const tabs = await chrome.tabs.query({
+      active: true,
+      currentWindow: true,
+    });
+    if (!tabs[0].url?.includes("linkedin")) {
+      setNotLinkedIn(true);
+    }
+  }, [setNotLinkedIn]);
+
+  React.useEffect(() => {
+    checkUrl();
+  }, [checkUrl]);
+
   return (
     <article className="w-full">
       {notLinkedIn ? (
-        <p className="w-full text-center text-red-500 text-base">
-          Extension only works in linkedIn
-        </p>
+        <>
+          <p className="w-full text-center text-red-500 text-base">
+            Extension only works in linkedIn
+          </p>
+          <span className="w-full flex justify-center items-center">
+            <a
+              href="https://www.linkedin.com/"
+              target="_blank"
+              className="text-blue-600 underline text-base"
+            >
+              https://www.linkedin.com/
+            </a>
+          </span>
+        </>
       ) : (
-        <ol className="w-full list-inside list-decimal text-base p-3">
-          {instructions.map((instruction, index) => {
-            return <li key={index}>{instruction}</li>;
+        <ol className="w-full list-inside list-decimal text-sm p-3">
+          {INSTRUCTIONS.map((instruction, index) => {
+            return (
+              <li key={index} className="py-2">
+                {instruction}
+              </li>
+            );
           })}
         </ol>
       )}
