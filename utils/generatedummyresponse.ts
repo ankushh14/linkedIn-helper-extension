bcb4a8d5dfb2fdf297fca26a88440a6c74e2042f
ankushh14 <ankushshenoy97@gmail.com>
2024-09-21 02:08:16 +0530

    docs: added JSDocs

--- utils/generatedummyresponse.ts
+++ utils/generatedummyresponse.ts
@@ -1,6 +1,15 @@
 import { CHAT_REPLY } from "@/constants";
 import { Chat } from "@/entrypoints/content/components/ChatAIComponent";
 
+/**
+ * Simulates an asynchronous response from a chat system after a delay.
+ *
+ * @param {Object} params - The input parameters.
+ * @param {string} params.text - The text input to be processed by the function.
+ * @returns {Promise<Chat>} A promise that resolves to a Chat object containing the reply text and author information.
+ *
+ */
+
 export const getResponse = async ({
   text,
 }: {
--- utils/utils.ts
+++ utils/utils.ts
@@ -1,6 +1,13 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
+/**
+ * Combines class names conditionally and merges Tailwind CSS classes, removing duplicates.
+ *
+ * @param {...ClassValue[]} inputs - A list of class name values that can include strings, arrays, or objects.
+ * @returns {string} A string containing the combined and merged class names.
+ */
+
+export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
