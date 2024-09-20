27b359eae82d98a15257466fb6aaacf0f847614a
ankushh14 <ankushshenoy97@gmail.com>
2024-09-20 20:46:52 +0530

    feat: scrollable chat component

--- entrypoints/content/components/ChatAIComponent.tsx
+++ entrypoints/content/components/ChatAIComponent.tsx
@@ -1,4 +1,5 @@
 import BounceLoader from "@/ui/bounce-loader";
+import { useEffect, useRef } from "react";
 
 type ChatAIComponentProps = {
   chats: Chat[];
@@ -6,11 +7,18 @@ type ChatAIComponentProps = {
 };
 
 const ChatAIComponent = ({ chats, loadingState }: ChatAIComponentProps) => {
+  const chatBodyRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (chatBodyRef.current) {
+      chatBodyRef.current.scrollTop = chatBodyRef.current.scrollHeight;
+    }
+  }, [chats]);
   return (
     <section
-      className={`w-full flex flex-col gap-4`}
+      className={`w-full flex flex-col gap-4 px-3 overflow-auto`}
       role="log"
       aria-live="polite"
+      ref={chatBodyRef}
     >
       {chats.map((chat, index) => {
         if (chat.author.toLowerCase() === "user") {
@@ -35,7 +43,7 @@ export type Chat = {
 
 const ChatMessage = ({ text }: Chat) => {
   return (
-    <div className="max-w-[80%] self-end flex flex-col justify-center items-center rounded-2xl p-4 bg-gray-300 animate-pop-in">
+    <div className="max-w-[80%] self-end flex flex-col justify-center items-center rounded-2xl p-4 pt-3 bg-gray-300 animate-pop-in">
       <p className="text-start whitespace-pre-wrap">{text}</p>
     </div>
   );
@@ -43,7 +51,7 @@ const ChatMessage = ({ text }: Chat) => {
 
 const ChatReply = ({ text }: { text: React.ReactNode; author: string }) => {
   return (
-    <div className="max-w-[80%] self-start flex flex-col justify-center items-center rounded-2xl p-4 bg-[#DBEAFE] animate-pop-in">
+    <div className="max-w-[80%] self-start flex flex-col justify-center items-center rounded-2xl p-4 pt-3 bg-[#DBEAFE] animate-pop-in">
       <p className="text-start whitespace-pre-wrap">{text}</p>
     </div>
   );
